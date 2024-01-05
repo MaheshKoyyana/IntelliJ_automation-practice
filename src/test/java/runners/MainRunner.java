1063420ba715e703f1a256726ebe35566da5a915
Mahesh <kmaheshmec@gmail.com>
2024-01-05 13:57:52 +0530

    Added lombok dependency and home feature

--- src/test/java/runners/MainRunner.java
+++ src/test/java/runners/MainRunner.java
@@ -5,7 +5,7 @@ import io.cucumber.testng.CucumberOptions;
 import org.testng.annotations.DataProvider;
 
 @CucumberOptions(features={"classpath:features"}, glue={"stepDefinitions"},
-monochrome = false, dryRun = false, tags = "@home1",
+monochrome = false, dryRun = false, tags = "",
         plugin = {"pretty","html:target/cucumber","json:target/cucumber.json"})
 public class MainRunner extends AbstractTestNGCucumberTests {
         @Override
